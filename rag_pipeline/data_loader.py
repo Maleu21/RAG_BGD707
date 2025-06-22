2b0007e145b543eaeabda9a5cac9a413401635d3
Maleu21 <mael.ats21@gmail.com>
2025-06-22 22:47:51 +0200

    Update

--- rag_pipeline/data_loader.py
+++
@@ -1,9 +0,0 @@
-# rag_pipeline/data_loader.py
-from datasets import load_dataset as hf_load_dataset
-
-def load_data():
-    print("📦 Chargement du dataset depuis Hugging Face...")
-    dataset = hf_load_dataset("neural-bridge/rag-dataset-12000", split="train")
-    docs = [{"context": row["context"]} for row in dataset if "context" in row]
-    print(f"✅ {len(docs)} documents chargés.")
-    return docs
